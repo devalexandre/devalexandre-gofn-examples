026a08f21cde81d38652891d8dd39b47374fc681
devalexandre <alexandre@indev.net.br>
2023-04-16 22:53:57 -0300

    feat: filter taco doce

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,27 @@
-package cmd
+package main
+
+import (
+	"github.com/devalexandre/gofn-examples/internal/http"
+	"github.com/devalexandre/gofn/array"
+	"strings"
+)
+
+func main() {
+
+	api := http.NewApi()
+	tacos, err := api.GetTacos()
+	if err != nil {
+		panic(err)
+	}
+
+	filteredTacos := array.Filter(tacos, func(taco http.Taco) bool {
+		return strings.Contains(taco.Description, "doce")
+	})
+
+	for _, taco := range filteredTacos {
+		println(taco.Description)
+	}
+
+	println(len(filteredTacos))
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devalexandre/gofn-examples
 
 go 1.20
+
+require github.com/devalexandre/gofn v0.0.1 // indirect
